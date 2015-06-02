35a207e6c2053209d90b986e7c820b5c81fd3238
Alex Kowalczuk <askowalczuk93@gmail.com>
2015-06-01 21:32:21 -0500

    initial commit, got attributes

---
+++ attributes.py
@@ -0,0 +1,69 @@
+import json
+import pickle
+
+def get_ambiences(outpath):
+    with open("yelp_academic_dataset_business.json") as f:
+        amb = []
+        for line in f:
+            data = json.loads(line)
+            if "Ambience" in data["attributes"].keys():
+                for c in data["attributes"]["Ambience"]:
+                    if c not in amb:
+                        amb.append(c)
+
+    with open(outpath, "w") as f:
+        pickle.dump(amb, f)
+
+def get_toplevel_key(key, outpath):
+    with open("yelp_academic_dataset_business.json") as f:
+        values = []
+        for line in f:
+            data = json.loads(line)
+            if data[key] not in values:
+                values.append(data[key])
+
+    with open(outpath, "w") as f:
+        pickle.dump(values, f)
+
+def get_good_for(outpath):
+    with open("yelp_academic_dataset_business.json") as f:
+        amb = []
+        for line in f:
+            data = json.loads(line)
+            if "Good For" in data["attributes"].keys():
+                for c in data["attributes"]["Good For"]:
+                    if c not in amb:
+                        amb.append(c)
+
+    with open(outpath, "w") as f:
+        pickle.dump(amb, f)
+
+
+def get_all_attributes_and_values(outpath):
+    values = {}
+    with open("yelp_academic_dataset_business.json") as f:
+        for line in f:
+            attrs = json.loads(line)["attributes"]
+            for attr, val in attrs.iteritems():
+                if attr in values.keys():
+                    values[attr]["count"] += 1
+                    if val not in values[attr]["values"] and not isinstance(val, dict):
+                        values[attr]["values"].append(val)
+                else:
+                    values[attr] = {
+                            "count" : 1,
+                            "values": []
+                            }
+                    if isinstance(val, dict):
+                        values[attr]["values"] = val.keys()
+                    else:
+                        values[attr]["values"].append(val)
+
+
+    for k,v in values.iteritems():
+        print "{} : {}".format(k,v)
+
+    with open(outpath, "w") as f:
+        pickle.dump(values, f)
+
+get_all_attributes_and_values("attributes")
